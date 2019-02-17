750b5df90adaa9c5c52d6c4b1912791da7dd4231
Bernhard Arnold <arnobaer@users.noreply.github.com>
2019-02-17 23:08:04 +0100

    Update Node.php

--- src/Node.php
+++ src/Node.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Summoning - Yet another HTML5 code generator.
- * Copyright (C) 2013,2018  Bernhard Arnold <bernhard.arnold@burgried.at>
+ * Copyright (C) 2013-2019  Bernhard Arnold <bernhard.arnold@burgried.at>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Version 0.5.0
+// Version 0.4.0
 
 namespace Summoning;
 
